aa51c4aa18c3528d38d0cb61b8045fedafca4d6a
Altair1234 <ultramarinebicycle@gmail.com>
2020-06-17 17:35:38 +0530

    After episode 3

--- Sadabahar Final/Scripts/Player.gd
+++ Sadabahar Final/Scripts/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody
 var direction: Vector3
 var velocity: Vector3
 export var speed:int = 20
-var i = 0
+var gravity = -1
 
 func _physics_process(delta):
 	process_input(delta)
@@ -14,21 +14,25 @@ func process_input(delta):
 	direction.z = 0
 	
 	if Input.is_action_pressed("walk_forward"):
-		direction.z -= 1
+		direction -= transform.basis.z
 	
 	if Input.is_action_pressed("walk_backward"):
-		direction.z += 1
+		direction += transform.basis.z
 	
 	if Input.is_action_pressed("walk_left") and Input.is_action_pressed("rotate_player"):
 		rotation_degrees.y += 2.5
 	elif Input.is_action_pressed("walk_left"):
-		direction.x -= 1
+		direction -= transform.basis.x
 	
 	if Input.is_action_pressed("walk_right") and Input.is_action_pressed("rotate_player"):
 		rotation_degrees.y -= 2.5
 	elif Input.is_action_pressed("walk_right"):
-		direction.x += 1
+		direction += transform.basis.x
 
 func process_movement(delta):
-	velocity = direction * speed
+	direction = direction.normalized()
+	if is_on_floor():
+		velocity = direction * speed
+	else:
+		velocity.y += gravity
 	move_and_slide(velocity, Vector3(0,1,0), 4)
